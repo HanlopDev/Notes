751916c314a0cead97df0197cb0eacb87840ac1c
HanlopDev <hanafbomar@gmail.com>
2023-11-07 08:54:32 +0300

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Hanaf on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
